fc0f761903b5be45cb6c408ec80cd9877f8d4aa2
Charlie Ramirez <shinobi@Charlies-MacBook-Pro.local>
2020-09-24 20:28:39 -0500

    agregar porcentaje al progress bar y una alarma al terminar el tiempo y llegar a 1 para el valor de progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var player: AVAudioPlayer!
+    
+    @IBOutlet weak var textEggs: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        textEggs.text = hardness
+        timer.invalidate()
+        
+        //print(sender.currentTitle)
 
-
+        print("Total Time in secs = \(eggTimes[hardness]!)")
+        
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print("Progress Bar value = \(Float(secondsPassed) / Float(totalTime))")
+        }
+        else {
+            timer.invalidate()
+            textEggs.text = "Eggs Done!"
+            playSound(soundName: "alarm_sound")
+        }
+        
+    }
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
+
